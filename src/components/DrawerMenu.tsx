dd817063f6e9ec16a296f497f734443d6ee1adb9
ErnestoCisnero <miguelernestocisnero@gmail.com>
2023-01-22 21:33:58 -0500

    Add new colors to drawer links

--- src/components/DrawerMenu.tsx
+++ src/components/DrawerMenu.tsx
@@ -5,31 +5,31 @@ export const DrawerMenu = () => {
     return (
         <aside className="flex flex-col min-w-[9.2%] border-r-[1px] border-slate-300 justify-between">
             <ul className="my-5 mx-2">
-                <li className="flex align-middle mb-2 px-1 py-3 rounded-sm font-semibold hover:bg-[#266663] hover:text-neutral-50">
+                <li className="flex align-middle mb-2 px-1 py-3 rounded-sm text-neutral-400s hover:bg-[#266663] hover:text-neutral-50">
                     <span className="material-symbols-outlined">dashboard</span>
                     <a href="#" className="ml-2">Dashboard</a>
                 </li>
-                <li className="flex align-middle mb-2 px-1 py-3 rounded-sm font-semibold hover:bg-[#266663] hover:text-neutral-50">
+                <li className="flex align-middle mb-2 px-1 py-3 rounded-sm text-neutral-400 hover:bg-[#266663] hover:text-neutral-50">
                 <span className="material-symbols-outlined">design_services</span>
                     <a href="#" className="ml-2">Services</a>
                 </li>
-                <li className="flex align-middle mb-2 px-1 py-3 rounded-sm font-semibold hover:bg-[#266663] hover:text-neutral-50">
+                <li className="flex align-middle mb-2 px-1 py-3 rounded-sm text-neutral-400 hover:bg-[#266663] hover:text-neutral-50">
                     <span className="material-symbols-outlined">currency_exchange</span>
                     <a href="#" className="ml-2">Exchange</a>
                 </li>
-                <li className="flex align-middle mb-2 px-1 py-3 rounded-sm font-semibold hover:bg-[#266663] hover:text-neutral-50">
+                <li className="flex align-middle mb-2 px-1 py-3 rounded-sm text-neutral-400 hover:bg-[#266663] hover:text-neutral-50">
                     <span className="material-symbols-outlined">credit_card</span>
                     <a href="#" className="ml-2">Cards</a>
                 </li>
-                <li className="flex align-middle mb-2 px-1 py-3 rounded-sm font-semibold hover:bg-[#266663] hover:text-neutral-50">
+                <li className="flex align-middle mb-2 px-1 py-3 rounded-sm text-neutral-400  hover:bg-[#266663] hover:text-neutral-50">
                     <span className="material-symbols-outlined">attach_money</span>
                     <a href="#" className="ml-2">Depostis</a>
                 </li>
-                <li className="flex align-middle mb-2 px-1 py-3 rounded-sm font-semibold hover:bg-[#266663] hover:text-neutral-50">
+                <li className="flex align-middle mb-2 px-1 py-3 rounded-sm text-neutral-400 hover:bg-[#266663] hover:text-neutral-50">
                     <span className="material-symbols-outlined">summarize</span>
                     <a href="#" className="ml-2">History</a>
                 </li>
-                <li className="flex align-middle mb-2 px-1 py-3 rounded-sm font-semibold hover:bg-[#266663] hover:text-neutral-50">
+                <li className="flex align-middle mb-2 px-1 py-3 rounded-sm text-neutral-400 hover:bg-[#266663] hover:text-neutral-50">
                     <span className="material-symbols-outlined">person</span>
                     <a href="#" className="ml-2">Account</a>
                 </li>
--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -10,7 +10,7 @@ export const MainLayout = ({ children }: MainLayoutPropType) => {
         <div className="flex flex-col h-screen">
             <nav className="flex justify-between min-w-[100%] border-b-[1px] border-slate-300">
                 <div className="p-3">
-                    <img src="FinFeed.svg" alt="name_page" />
+                    <img src="FinFeed.svg" alt="name_page" className="w-28"/>
                 </div>
                 
                 <UserProfile />
