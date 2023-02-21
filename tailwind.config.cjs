e3767fe2432667cc480d1aec8bb14851f7409910
ErnestoCisnero <miguelernestocisnero@gmail.com>
2023-02-20 21:22:47 -0500

    Purge tailwind file to deploy

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,jsx,ts,tsx}"],
+  purge: [
+    "./src/components/**/*.{js,jsx}",
+    "./public/index.html",
+  ],
   theme: {
     extend: {},
   },
