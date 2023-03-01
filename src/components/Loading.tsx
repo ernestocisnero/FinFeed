3e567c2484a3d420b06ab4ea08d4a954313cc22f
ErnestoCisnero <miguelernestocisnero@gmail.com>
2023-02-28 22:06:06 -0500

    Add loading spin

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,6 +2,12 @@
 
 export const Loading = () => {
     return (
-        <div>Loading...</div>
+        <div className="p-4">
+            <div className="loader-wrapper">
+                <div className="loader">
+                    <div className="loader loader-inner"></div>
+                </div>
+            </div>
+        </div>
     )
 }
